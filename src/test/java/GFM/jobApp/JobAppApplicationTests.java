5874a98ed22fb056318414baf07165869ae15bc8
SafdarThaheem <safdarthaheem716@gmail.com>
2024-11-16 20:36:57 +0500

    initial spring-boot project

---
+++ src/test/java/GFM/jobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package GFM.jobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
