a2e553df84af6d94d8da3660b214271839da719a
Safdar Ullah Thaheem <s.thaheem@globalfinancialmedia.com>
2024-12-02 17:48:59 +0500

    JPA implementation create Job table in h2-DB

--- src/main/java/GFM/jobApp/job/impl/JobServiceImpl.java
+++ src/main/java/GFM/jobApp/job/impl/JobServiceImpl.java
@@ -2,56 +2,58 @@ package GFM.jobApp.job.impl;
 
 import GFM.jobApp.job.JobService;
 import GFM.jobApp.job.models.Job;
+import GFM.jobApp.job.repo.JobRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
 
-    private final List<Job> jobs = new ArrayList<>();
+//    private final List<Job> jobs = new ArrayList<>();
+
+    JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
     private Long nextId = 1L;
 
     @Override
     public List<Job> findJobs() {
-        return jobs;
+        return jobRepository.findAll();
     }
 
     @Override
     public void createJob(Job job) {
         job.setId(nextId++);
-        jobs.add(job);
+        jobRepository.save(job);
     }
 
     @Override
     public Job getJobById(Long id) {
-        for (Job job : jobs) {
-            if (job.getId().equals(id)) {
-                return job;
-            }
-        }
-        return null;
+        return jobRepository.findById(id).orElse(null);
     }
 
     @Override
     public boolean deleteJobById(Long id) {
-        Iterator<Job> iterator = jobs.iterator();
-        while (iterator.hasNext()) {
-            Job job = iterator.next();
-            if (job.getId().equals(id)) {
-                iterator.remove();
-                return true;
-            }
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
 
     @Override
     public boolean updateJob(Long id, Job updatedJob) {
-        for (Job job : jobs) {
-            if (job.getId().equals(id)) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+            if (jobOptional.isPresent()) {
+                Job job = jobOptional.get();
                 job.setTitle(updatedJob.getTitle());
                 job.setDescription(updatedJob.getDescription());
                 job.setJobType(updatedJob.getJobType());
@@ -60,7 +62,6 @@ public class JobServiceImpl implements JobService {
                 job.setMinSalary(updatedJob.getMinSalary());
                 return true;
             }
-        }
         return false;
     }
 }
